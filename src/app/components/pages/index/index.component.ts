3bd4955464c92d127f3ff322c8317f5cd732a593
Lucas da Silva Souto <lucas.souto@ibm.com>
2018-04-18 09:27:57 -0300

    fixed slick version on local

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Simple } from '../../../interfaces/items/simple';
 import { ItemService } from '../../../services/item/item.service';
 
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -29,29 +30,25 @@ export class IndexComponent implements OnInit {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5
+            slidesToShow: 5
           }
         },
         {
           breakpoint: 900,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToShow: 3
           }
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 2
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 1
           }
         }
       ]
